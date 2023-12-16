1474b648b5e8221a3076297e2d208e1c859219ee
kaoruyuki <>
2023-12-16 14:25:49 +0900

    fix

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ from common import set_log
 from logging import getLogger, config
 
 
-if __name__ == '__main__':
-    log_conf = set_log()
-    config.dictConfig(log_conf)
-    logger = getLogger(__name__)
+log_conf = set_log()
+config.dictConfig(log_conf)
+logger = getLogger(__name__)
 
+
+if __name__ == '__main__':
     logger.info('Testing xxx.')
