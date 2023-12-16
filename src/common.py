8ba88c612c732e6aaa76b7ac89a83c2362cec836
kaoruyuki <>
2023-12-16 18:57:11 +0900

    Add conf

--- src/common.py
+++ src/common.py
@@ -2,6 +2,13 @@ import json
 from datetime import datetime
 
 
+def set_common():
+    with open('./conf/common.json', 'r') as f:
+        common_conf = json.load(f)
+
+    return common_conf
+
+
 def set_log():
     with open('./conf/log.json', 'r') as f:
         log_conf = json.load(f)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from common import set_log
+from common import set_log, set_common
 from logging import getLogger, config
 
 
@@ -6,6 +6,7 @@ log_conf = set_log()
 config.dictConfig(log_conf)
 logger = getLogger(__name__)
 
+common_conf = set_common()
 
 if __name__ == '__main__':
-    logger.info('Testing xxx.')
+    logger.info(common_conf['AAA'])
